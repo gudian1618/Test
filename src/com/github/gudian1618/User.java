e6b56faf9cba0326142ec5e49b8232801831f361
gudian1618 <1761844211@qq.com>
2019-04-15 16:18:43 +0800

    新建

---
+++ src/com/github/gudian1618/User.java
@@ -0,0 +1,51 @@
+package com.github.gudian1618;
+
+/**
+ * @param
+ * @author gudian1618
+ * @version v1.0
+ * @company www.github.com
+ * @date 2019/4/15 16:14
+ * @description
+ *
+ *
+ */
+public class User {
+    
+    private String username;
+    private String password;
+    private int age;
+    private String sex;
+    
+    public String getUsername() {
+        return username;
+    }
+    
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    
+    public String getPassword() {
+        return password;
+    }
+    
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    public int getAge() {
+        return age;
+    }
+    
+    public void setAge(int age) {
+        this.age = age;
+    }
+    
+    public String getSex() {
+        return sex;
+    }
+    
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+}
