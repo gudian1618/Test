c6200e5f187d499df9b3c5b7c2d6d8d741e9093a
gudian1618 <1761844211@qq.com>
2019-05-01 23:39:54 +0800

    新修改

--- src/com/github/gudian1618/Java_2/StringDemo.java
+++ src/com/github/gudian1618/Java_2/StringDemo.java
@@ -7,6 +7,8 @@ package com.github.gudian1618.Java_2;
  * @date 2019-05-01 22:11
  * @description
  * String 表示一个字符串，内部使用字符数组实现，不能被继承的最终类
+ * 字符串常量池
+ * String类编译器与运行期的分析，四种情况
  *
  */
 
@@ -24,25 +26,25 @@ public class StringDemo {
         System.out.println(s1);
         System.out.println(s2);
         System.out.println(s1==s2);
-        // 直接赋值字符串连接时，考虑编译期和运行期
+        // 1.直接赋值字符串连接时，考虑编译期和运行期
         // 如果在编译期可以被确定（常量可以被确定，变量不能被确定），那么就使用已有的对象，否则会创建新的对象
         // 变量不可以确定
         String a = "a";
         String a1 = a+1;
         String a2 = "a1";
         System.out.println(a1==a2);
-        // 常量可以确定
+        // 2.常量可以确定
         final String b = "b";
         String b1 = b+"1";
         String b2 = "b1";
         System.out.println(b1==b2);
-        
+        // 3.方法必须在运行期才能得到具体值，相当于通过方法创建了一个常量池，但不确定，必须是运行期方法才能确定具体值
         String c = getC();
         String c1 = c+"1";
         String c2 = "c1";
         System.out.println(c1==c2);
-
-        String d = getD();
+        // 4.即使final修饰这是最后的实现，不可再改，但是方法必须在运行期才能获得具体值，即不确定，会创建新的堆内存。
+        final String d = getD();
         String d1 = d+1;
         String d2 = "d1";
         System.out.println(d1==d2);
