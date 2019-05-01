1c87687c415b561613434bb85f3d125b7caf6c81
gudian1618 <1761844211@qq.com>
2019-05-01 22:14:54 +0800

    新建的Java学习

---
+++ src/com/github/gudian1618/Java_2/StringDemo.java
@@ -0,0 +1,25 @@
+package com.github.gudian1618.Java_2;
+
+/**
+ * @param
+ * @author gudian1618
+ * @version 1.0
+ * @date 2019-05-01 22:11
+ * @description
+ *
+ *
+ */
+
+public class StringDemo {
+
+    public static void main(String[] args) {
+
+        // String 的两种赋值方式，推荐使用第一种方式
+        // 1.直接赋值
+        String s1 = "奋斗";
+        // 2.使用new关键字创建对象
+        String s2 = new String("粪粪");
+
+
+    }
+}
