286273ab78292ebd0b337bc2267a7b8e430c2b97
gudian1618 <1761844211@qq.com>
2019-05-01 23:26:31 +0800

    新修改

--- src/com/github/gudian1618/Java_2/StringDemo.java
+++ src/com/github/gudian1618/Java_2/StringDemo.java
@@ -24,13 +24,36 @@ public class StringDemo {
         System.out.println(s1);
         System.out.println(s2);
         System.out.println(s1==s2);
-
+        // 直接赋值字符串连接时，考虑编译期和运行期
+        // 如果在编译期可以被确定（常量可以被确定，变量不能被确定），那么就使用已有的对象，否则会创建新的对象
+        // 变量不可以确定
         String a = "a";
         String a1 = a+1;
         String a2 = "a1";
         System.out.println(a1==a2);
+        // 常量可以确定
+        final String b = "b";
+        String b1 = b+"1";
+        String b2 = "b1";
+        System.out.println(b1==b2);
+        
+        String c = getC();
+        String c1 = c+"1";
+        String c2 = "c1";
+        System.out.println(c1==c2);
 
-
-
+        String d = getD();
+        String d1 = d+1;
+        String d2 = "d1";
+        System.out.println(d1==d2);
     }
+
+    private static String getC() {
+        return "c";
+    }
+
+    private static String getD() {
+        return "d";
+    }
+
 }
