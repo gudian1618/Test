bf5c57b7224f4ec99e149319011af0c2bd698945
gudian1618 <1761844211@qq.com>
2019-05-03 22:50:04 +0800

    新修改

--- src/com/github/gudian1618/Java_2/MathDemo.java
+++ src/com/github/gudian1618/Java_2/MathDemo.java
@@ -1,7 +1,9 @@
 package com.github.gudian1618.Java_2;
 
-import static java.lang.Math.*;
+import java.util.Random;
 
+import static java.lang.Math.*;
+// 静态导入
 /**
  * @param
  * @author gudian1618
@@ -14,7 +16,13 @@ public class MathDemo {
     public static void main(String[] args) {
         System.out.println(PI);
         System.out.println(abs(-10));
-        System.out.println(random()*100);
+        System.out.println(round(random()*100));
+        System.out.println(sqrt(3));
+        System.out.println(floor(1.234));
+
+        Random r = new Random();
+        System.out.println(r.nextLong());
+        System.out.println(r.nextInt());
 
     }
 
