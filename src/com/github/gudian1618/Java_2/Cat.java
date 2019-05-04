4157f202092246adb0176bdadf8d7548ce837553
gudian1618 <1761844211@qq.com>
2019-05-04 09:32:00 +0800

    新修改

---
+++ src/com/github/gudian1618/Java_2/Cat.java
@@ -0,0 +1,33 @@
+package com.github.gudian1618.Java_2;
+
+/**
+ * @param
+ * @author gudian1618
+ * @version 1.0
+ * @date 2019-05-04 09:21
+ * @description
+ */
+
+public class Cat {
+
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+
+}
---
+++ src/com/github/gudian1618/Java_2/Test_15.java
@@ -0,0 +1,25 @@
+package com.github.gudian1618.Java_2;
+
+import java.util.Arrays;
+
+/**
+ * @param
+ * @author gudian1618
+ * @version 1.0
+ * @date 2019-05-04 09:17
+ * @description
+ */
+
+public class Test_15 {
+    public static void main(String[] args) {
+
+        int[] nums = {32,45,66,23,5,1,98,54,12};
+        Arrays.sort(nums);
+        System.out.println(Arrays.toString(nums));
+
+        String[] names = {"jack","tom","菲菲","粪粪"};
+        Arrays.sort(names);
+        System.out.println(Arrays.toString(names));;
+
+    }
+}
