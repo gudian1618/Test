3bbed8dea13c5fba43bf1be0640a81c35ed6b571
gudian1618 <1761844211@qq.com>
2019-05-10 22:22:41 +0800

    新修改

--- src/com/github/gudian1618/Java_2/StringBufferDemo.java
+++ src/com/github/gudian1618/Java_2/StringBufferDemo.java
@@ -14,6 +14,16 @@ public class StringBufferDemo {
         String b = "b";
         String c = a+b+1;
         System.out.println(c);
+
+        String d = "a"+1+2+3+4+"b";
+
+        // StringBuffer目的是来将解决字符串相加时带来的性能问题（常量与变量）
+        // StringBuffer的内部实现采用字符数组，默认数组的长度为16，超过数组大小时，动态扩充的算法是原来长度*2+2
+        // 所以当我们预知要添加的数据长度时，建议使用带初始化容量的构造方法，来避免动态扩充的次数，从而提高效率。
+
+        StringBuffer sb = new StringBuffer();
+        sb.append(a).append(b).append(1);
+        System.out.println(sb.toString());
     }
 }
 
