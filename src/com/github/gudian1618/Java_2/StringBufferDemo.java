f48e9ba791781f6d98e9796f5b645c14ca40b0ce
gudian1618 <1761844211@qq.com>
2019-05-10 22:06:24 +0800

    新修改

---
+++ src/com/github/gudian1618/Java_2/StringBufferDemo.java
@@ -0,0 +1,20 @@
+package com.github.gudian1618.Java_2;
+
+/**
+ * @param
+ * @author gudian1618
+ * @version 1.0
+ * @date 2019-05-10 22:01
+ * @description
+ */
+
+public class StringBufferDemo {
+    public static void main(String[] args) {
+        String a = "a";
+        String b = "b";
+        String c = a+b+1;
+        System.out.println(c);
+    }
+}
+
+
