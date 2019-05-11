c787642cb06c4fba0e2892ebf8338e15eec74335
gudian1618 <1761844211@qq.com>
2019-05-11 21:39:19 +0800

    新建

--- src/com/github/gudian1618/Java_2/I18NDemo.java
+++ src/com/github/gudian1618/Java_2/I18NDemo.java
@@ -1,6 +1,8 @@
 package com.github.gudian1618.Java_2;
 
 import java.util.Locale;
+import java.util.ResourceBundle;
+import java.util.Scanner;
 
 /**
  * @param
@@ -21,6 +23,21 @@ public class I18NDemo {
         // 获取当前系统默认的语言环境
         Locale locale_defualt = Locale.getDefault();
 
+        Scanner input = new Scanner(System.in);
+        // 用于绑定属性文件的工具类（参数：属性文件的基本名）
+        ResourceBundle r = ResourceBundle.getBundle(("com.github.gudian1618" +
+                ".Java_2.info"),locale_US);
+        System.out.println(r.getString("system.name"));
+        System.out.println(r.getString("input.username"));
+        String username = input.nextLine();
+        System.out.println(r.getString("input.password"));
+        String password = input.nextLine();
 
+        if ("admin".equals(username) && "123".equals(password)) {
+            System.out.println(r.getString("login.success"));
+        } else {
+            System.out.println(r.getString("login.error"));
+
+        }
     }
 }
