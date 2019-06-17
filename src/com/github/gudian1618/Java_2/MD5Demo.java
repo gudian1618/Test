e69320410effa20b10aaf6480a702fbde2160dca
gudian1618 <1761844211@qq.com>
2019-06-17 22:35:40 +0800

    新建

--- src/com/github/gudian1618/Java_2/MD5Demo.java
+++ src/com/github/gudian1618/Java_2/MD5Demo.java
@@ -4,6 +4,7 @@ import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
+import java.util.Base64;
 
 /**
  * @param
@@ -16,6 +17,7 @@ public class MD5Demo {
     public static void main(String[] args) {
         
         String password = "123456";
+        String savePassword = "4QrcOUm6Wau+VuBX8g+IPg==";
         
         try {
             MessageDigest md = MessageDigest.getInstance("md5");
@@ -23,6 +25,14 @@ public class MD5Demo {
             byte[] bytes = md.digest(password.getBytes(StandardCharsets.UTF_8));
             System.out.println(Arrays.toString(bytes));
             String mdStr = new String(bytes);
+            System.out.println(mdStr);
+            // 1.8版本之后，编解码的方法
+            String str = Base64.getEncoder().encodeToString(bytes);
+            System.out.println(str);
+            
+            byte[] str1 = Base64.getDecoder().decode(str);
+            System.out.println(Arrays.toString(str1));
+            
         } catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
         }
