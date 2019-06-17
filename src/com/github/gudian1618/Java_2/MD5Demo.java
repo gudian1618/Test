3f94550472fdb8786f6ab3172ee1fbb56308f723
gudian1618 <1761844211@qq.com>
2019-06-17 22:51:18 +0800

    新建

--- src/com/github/gudian1618/Java_2/MD5Demo.java
+++ src/com/github/gudian1618/Java_2/MD5Demo.java
@@ -14,8 +14,38 @@ import java.util.Base64;
  * @description
  */
 public class MD5Demo {
+    // 存储的密文
+    private static String savePassword = "4QrcOUm6Wau+VuBX8g+IPg==";
     public static void main(String[] args) {
-        
+    
+//        test();
+        System.out.println(login("12345678"));
+    
+    }
+    
+    private static boolean login(String password) {
+        if(savePassword.equals(md5(password))) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    // 计算MD5的工具方法
+    private static String md5(String password) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("md5");
+            // 通过md5计算摘要
+            byte[] bytes = md.digest(password.getBytes(StandardCharsets.UTF_8));
+            String str2 = Base64.getEncoder().encodeToString(bytes);
+            return str2;
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    
+    private static void test() {
         String password = "123456";
         String savePassword = "4QrcOUm6Wau+VuBX8g+IPg==";
         
@@ -36,7 +66,6 @@ public class MD5Demo {
         } catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
         }
-    
     }
     
 }
