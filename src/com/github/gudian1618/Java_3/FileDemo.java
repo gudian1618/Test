1a12dabbc3a8af1a163734e250e8ff1db4593274
gudian1618 <1761844211@qq.com>
2019-06-18 17:06:16 +0800

    新建

--- src/com/github/gudian1618/Java_3/FileDemo.java
+++ src/com/github/gudian1618/Java_3/FileDemo.java
@@ -1,15 +1,22 @@
 package com.github.gudian1618.Java_3;
 
+import java.io.File;
+
 /**
  * @param
  * @author gudian1618
  * @version v1.0
  * @date 2019/6/18 16:59
  * @description
+ * File类
  */
 public class FileDemo {
     
     public static void main(String[] args) {
-    
+        // FIle类表示一个文件或目录
+        File f1 = new File("F:/ProgramData/Test/src/com/github/gudian1618/Java_3/lianxi.txt");
+        // 或者\\表示转义字符写也行，
+        // F:/ProgramData/Test/src/com/github/gudian1618/Java_3/lianxi.txt"
+        
     }
 }
