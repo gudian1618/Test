678cb73decff25e44612a29efd02340a7221ded5
gudian1618 <1761844211@qq.com>
2019-06-18 11:14:16 +0800

    新建

---
+++ src/com/github/gudian1618/Java_2/LambdaDemo.java
@@ -0,0 +1,48 @@
+package com.github.gudian1618.Java_2;
+
+/**
+ * @param
+ * @author gudian1618
+ * @version v1.0
+ * @date 2019/6/18 10:59
+ * @description
+ * 只有一个方法的类，函数作为参数传递，或者将代码看成数据。
+ */
+public class LambdaDemo {
+    public static void main(String[] args) {
+        
+        IEat ieat = new IEatImpl();
+        ieat.eat();
+        
+        IEat ieat2 = new IEat() {
+            @Override
+            public void eat() {
+                System.out.println("eat banana");
+            }
+        };
+        ieat2.eat();
+        
+        // Lambda表达式（类或者接口只有一个方法时），具体只有一句代码时，可以省略大括号的代码块。
+        // 好处：代码简洁，不会单独生成class文件。
+//        IEat ieat3 = ()->System.out.println("eat apple banana");
+        IEat ieat3 = ()-> {
+            System.out.println("eat apple banana");
+        };
+        
+        ieat3.eat();
+
+    
+    }
+}
+
+// 只有一个抽象方法的接口
+interface IEat {
+    public void eat();
+}
+
+class IEatImpl implements IEat {
+    @Override
+    public void eat() {
+        System.out.println("eat apple");
+    }
+}
