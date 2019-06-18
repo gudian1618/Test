8fa12498bfec046bae7994e17e4a55a08bf0bf63
gudian1618 <1761844211@qq.com>
2019-06-18 16:17:09 +0800

    新建

---
+++ src/com/github/gudian1618/Java_2/BinaryTrreeDemo.java
@@ -0,0 +1,17 @@
+package com.github.gudian1618.Java_2;
+
+/**
+ * @param
+ * @author gudian1618
+ * @version v1.0
+ * @date 2019/6/18 16:15
+ * @description
+ * 树是一种非线性数据结构。
+ * 第一个元素为根节点
+ * 元素大于根节点的房右子树，小于的放左子树。
+ */
+public class BinaryTrreeDemo {
+    public static void main(String[] args) {
+    
+    }
+}
