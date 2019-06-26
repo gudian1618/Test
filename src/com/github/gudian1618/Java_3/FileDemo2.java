1449b21b20cdc92e47edc8a5e7108380552d4d02
gudian1618 <1761844211@qq.com>
2019-06-26 21:51:46 +0800

    新建

--- src/com/github/gudian1618/Java_3/FileDemo2.java
+++ src/com/github/gudian1618/Java_3/FileDemo2.java
@@ -1,5 +1,7 @@
 package com.github.gudian1618.Java_3;
 
+import java.io.File;
+
 /**
  * @param
  * @author gudian1618
@@ -12,6 +14,23 @@ package com.github.gudian1618.Java_3;
 public class FileDemo2 {
     
     public static void main(String[] args) {
+        findFile(new File("D:\\"), ".jpg");
     
+    }
+    
+    private static void findFile(File target, String ext) {
+        if (target==null) return;
+        if (target.isDirectory()) {
+            File[] files = target.listFiles();
+            if (files!=null) {
+                for (File f:files) {
+                    findFile(f,ext);
+                }
+            }
+        } else {
+            // 此处表示File是一个文件
+            target.getName()
+        
+        }
     }
 }
