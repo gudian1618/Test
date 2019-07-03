1cf7f4d82ed6230b5959727f6f06c73318702ea5
gudian1618 <1761844211@qq.com>
2019-07-03 23:59:14 +0800

    新建

--- src/com/github/gudian1618/Java_3/CopyFileDemo.java
+++ src/com/github/gudian1618/Java_3/CopyFileDemo.java
@@ -13,26 +13,38 @@ import java.io.*;
 public class CopyFileDemo {
 
     public static void main(String[] args) {
-
+        System.out.println("start copy...");
+        copy("/Users/zyd/IdeaProjects/Test/src/com/github/gudian1618/Java_3" +
+            "/lianxi.txt","/Users/zyd/IdeaProjects/Test/src/com/github" +
+            "/gudian1618/Java_3/dabin/copy.txt");
+        System.out.println("copy success!");
     }
 
     private static void copy(String src, String target) {
         File srcfile = new File(src);
         File targetfile = new File(target);
+        InputStream in = null;
+        OutputStream out = null;
         try {
-            InputStream in = new FileInputStream(srcfile);
-            OutputStream out = new FileOutputStream(targetfile);
+            in = new FileInputStream(srcfile);
+            out = new FileOutputStream(targetfile);
 
             byte[] bytes = new byte[256];
             int len = -1;
             while ((len=in.read(bytes))!=-1) {
                 out.write(bytes,0,len);
-
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
+        } finally {
+            try {
+                if (in!=null) in.close();
+                if (out!=null) out.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 
