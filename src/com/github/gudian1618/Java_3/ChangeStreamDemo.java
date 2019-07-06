245903dfb78bde2b70efe199025a9842f15e6337
gudian1618 <1761844211@qq.com>
2019-07-06 22:45:15 +0800

    新建

---
+++ src/com/github/gudian1618/Java_3/ChangeStreamDemo.java
@@ -0,0 +1,20 @@
+package com.github.gudian1618.Java_3;
+
+/**
+ * @param
+ * @author gudian1618
+ * @version 1.0
+ * @date 2019-07-06 22:41
+ * @description
+ * 转换流：
+ * OutputStreamWriter：可以将输出的字符流转换为字节流的输出形式
+ * InputStreamReader：将输入的字节流转换为字符流输入形式
+ *
+ */
+
+public class ChangeStreamDemo {
+
+    public static void main(String[] args) {
+
+    }
+}
