c9743a7714374de9b7ec3caf1efb96c9619045dc
gudian1618 <1761844211@qq.com>
2019-07-06 22:51:06 +0800

    新建

--- src/com/github/gudian1618/Java_3/ChangeStreamDemo.java
+++ src/com/github/gudian1618/Java_3/ChangeStreamDemo.java
@@ -1,5 +1,11 @@
 package com.github.gudian1618.Java_3;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.nio.charset.Charset;
+
 /**
  * @param
  * @author gudian1618
@@ -13,6 +19,21 @@ package com.github.gudian1618.Java_3;
  */
 
 public class ChangeStreamDemo {
+
+    private static void in(InputStream in) {
+        Reader reader = new InputStreamReader(in, Charset.forName("UTF-8"));
+        char[] cs = new char[128];
+        int len = -1;
+        try {
+            while ((len = reader.read(cs)) != -1) {
+                System.out.println(new String(cs, 0, len));
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 
     public static void main(String[] args) {
 
