906f356127efa4f47c668dcefe4b53df786133cc
gudian1618 <1761844211@qq.com>
2019-07-09 23:25:07 +0800

    新建

--- src/com/github/gudian1618/Java_3/BufferStreamDemo.java
+++ src/com/github/gudian1618/Java_3/BufferStreamDemo.java
@@ -1,6 +1,9 @@
 package com.github.gudian1618.Java_3;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
 
 /**
  * @param
@@ -13,7 +16,13 @@ import java.io.File;
 public class BufferStreamDemo {
 
     private static void byteWriter() {
-        File file = new File("")
+        File file = new File("/Users/zyd/IdeaProjects/Test/src/com/github" +
+            "/gudian1618/Java_3/lianxi3.txt");
+        try {
+            OutputStream out = new FileOutputStream(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
     public static void main(String[] args) {
 
