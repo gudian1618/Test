28957024e8599a1faa5a6945c29a7398f961fe79
gudian1618 <1761844211@qq.com>
2019-07-09 23:52:21 +0800

    新建

--- src/com/github/gudian1618/Java_3/BufferStreamDemo.java
+++ src/com/github/gudian1618/Java_3/BufferStreamDemo.java
@@ -8,10 +8,11 @@ import java.io.*;
  * @version 1.0
  * @date 2019-07-09 23:21
  * @description
- * 缓存的目的：
+ * 缓存的目的：以后写文件要用缓存的方式
  * 解决在写入文件操作时，频繁的操作文件所带来的性能降低的问题。
  * BufferOutputStream 内部默认的缓存大小是8KB，每次写入时存储到缓存中的byte
  * 数组中，当数组存满时，会把数组中的数据写入到文件中，并且缓存下标归零。
+ * 可以通过实现抽象类cloneable，在try语句中直接写条件，自动关闭。
  */
 
 public class BufferStreamDemo {
@@ -19,17 +20,14 @@ public class BufferStreamDemo {
     private static void byteReader() {
         File file = new File("/Users/zyd/IdeaProjects/Test/src/com/github" +
             "/gudian1618/Java_3/lianxi3.txt");
-        try {
-            InputStream in = new FileInputStream(file);
-            // 构造一个字节缓冲流
-            BufferedInputStream bis = new BufferedInputStream(in);
+        try (BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file))) {
 
             byte[] bytes = new byte[1024];
             int len = -1;
             while ((len=bis.read(bytes))!=-1) {
                 System.out.println(new String(bytes,0,len));
             }
-            bis.close();
+
             // out.close();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
