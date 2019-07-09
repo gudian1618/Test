abb09477e69932c120a81d7cef8b23d429d9e24f
gudian1618 <1761844211@qq.com>
2019-07-09 23:22:36 +0800

    新建

---
+++ src/com/github/gudian1618/Java_3/BufferStreamDemo.java
@@ -0,0 +1,21 @@
+package com.github.gudian1618.Java_3;
+
+import java.io.File;
+
+/**
+ * @param
+ * @author gudian1618
+ * @version 1.0
+ * @date 2019-07-09 23:21
+ * @description
+ */
+
+public class BufferStreamDemo {
+
+    private static void byteWriter() {
+        File file = new File("")
+    }
+    public static void main(String[] args) {
+
+    }
+}
