a24f6850b1b1370f10f958a44ddecd69d7638a4b
gudian1618 <1761844211@qq.com>
2019-07-09 23:16:58 +0800

    新建

--- src/com/github/gudian1618/Java_3/ChangeStreamDemo.java
+++ src/com/github/gudian1618/Java_3/ChangeStreamDemo.java
@@ -9,10 +9,11 @@ import java.nio.charset.Charset;
  * @version 1.0
  * @date 2019-07-06 22:41
  * @description
+ * 字节流转换成字符流。
  * 转换流：输入输出都是相对程序而言，流入程序叫输入，流出程序叫输出。
  * OutputStreamWriter：可以将输出的字符流转换为字节流的输出形式，
  * InputStreamReader：将输入的字节流转换为字符流输入形式
- * 代码的表现形式是字节流转换成字符流。
+ * 代码的表现形式是字节流转换成字符流，也就是字节流封装成字符流。
  *
  */
 
