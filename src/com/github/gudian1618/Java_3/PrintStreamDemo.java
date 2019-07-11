8a4c3b6bfce79e8f70260bab90a42d5026279cc0
gudian1618 <1761844211@qq.com>
2019-07-12 00:10:43 +0800

    新建

--- src/com/github/gudian1618/Java_3/PrintStreamDemo.java
+++ src/com/github/gudian1618/Java_3/PrintStreamDemo.java
@@ -17,6 +17,25 @@ import java.io.*;
 
 public class PrintStreamDemo {
 
+    private static void CharPrint() {
+        try {
+            File file = new File("/Users/zyd/IdeaProjects/Test/src/com/github" +
+                "/gudian1618/Java_3/lianxi4");
+            Writer out = new FileWriter(file);
+            // 加缓存
+            BufferedWriter bos = new BufferedWriter(out);
+            // 增强打印功能
+            PrintWriter pw = new PrintWriter(bos);
+            pw.println("小河流水天天哗啦啦");
+            pw.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
     private static void bytePrint() {
         try {
             File file = new File("/Users/zyd/IdeaProjects/Test/src/com/github" +
