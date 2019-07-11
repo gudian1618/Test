a7431cbd1355639baaf53f262005eb75ff6ad802
gudian1618 <1761844211@qq.com>
2019-07-11 23:49:27 +0800

    新建

---
+++ src/com/github/gudian1618/Java_3/PrintStreamDemo.java
@@ -0,0 +1,18 @@
+package com.github.gudian1618.Java_3;
+
+/**
+ * @param
+ * @author gudian1618
+ * @version 1.0
+ * @date 2019-07-11 23:48
+ * @description
+ */
+
+public class PrintStreamDemo {
+
+    private static void print
+
+    public static void main(String[] args) {
+
+    }
+}
