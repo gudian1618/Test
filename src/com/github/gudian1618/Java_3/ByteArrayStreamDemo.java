c2360bfb4602d4a10ed69098285bd856cb0b4c5b
gudian1618 <1761844211@qq.com>
2019-07-14 01:01:13 +0800

    新建

--- src/com/github/gudian1618/Java_3/ByteArrayStreamDemo.java
+++ src/com/github/gudian1618/Java_3/ByteArrayStreamDemo.java
@@ -1,6 +1,7 @@
 package com.github.gudian1618.Java_3;
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 
 /**
  * @param
@@ -16,10 +17,11 @@ public class ByteArrayStreamDemo {
 
         String s = "1234567890gfdgfds(*$#@&^%^&SSFD";
         ByteArrayInputStream bais = new ByteArrayInputStream(s.getBytes());
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
         int curr = -1;
         while ((curr=bais.read())!=-1) {
-            if ((curr>=65 && curr <=) || (curr>=97 && curr<=122)) {
-
+            if ((curr>=65 && curr <=90) || (curr>=97 && curr<=122)) {
+                baos.write(curr);
             }
         }
     }
