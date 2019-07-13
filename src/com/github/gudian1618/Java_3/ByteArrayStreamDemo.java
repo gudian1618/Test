2b77168e3f9e3c981075eb54150add9bbe3a01ef
gudian1618 <1761844211@qq.com>
2019-07-14 00:55:20 +0800

    新建

--- src/com/github/gudian1618/Java_3/ByteArrayStreamDemo.java
+++ src/com/github/gudian1618/Java_3/ByteArrayStreamDemo.java
@@ -1,5 +1,7 @@
 package com.github.gudian1618.Java_3;
 
+import java.io.ByteArrayInputStream;
+
 /**
  * @param
  * @author gudian1618
@@ -11,6 +13,8 @@ package com.github.gudian1618.Java_3;
 public class ByteArrayStreamDemo {
 
     private static void byteArray() {
+        String s = "1234567890gfdgfds(*$#@&^%^&SSFD";
+        ByteArrayInputStream bis = new ByteArrayInputStream(s.getBytes());
 
     }
 
