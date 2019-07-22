5dbf7c3893ec59c83e6833ae6f1095ff512dc1c8
gudian1618 <1761844211@qq.com>
2019-07-22 15:59:52 +0800

    新建

--- src/com/github/gudian1618/Java_3/ObjectStreamDemo.java
+++ src/com/github/gudian1618/Java_3/ObjectStreamDemo.java
@@ -1,5 +1,7 @@
 package com.github.gudian1618.Java_3;
 
+import java.io.*;
+
 /**
  * @param
  * @author gudian1618
@@ -12,7 +14,17 @@ package com.github.gudian1618.Java_3;
 public class ObjectStreamDemo {
 
     private static void writerObject() {
-
+        Dog dog = new Dog("wangwang",2,"母");
+        File file = new File("/Users/zyd/IdeaProjects/Test/src/com/github" +
+            "/gudian1618/Java_3/dog.obj");
+        try {
+            OutputStream out = new FileOutputStream(file);
+            ObjectOutputStream oos = new ObjectOutputStream(out);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
