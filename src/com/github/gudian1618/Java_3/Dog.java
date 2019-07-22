bb0fb899c1804e50722363ca76bff54da61d251f
gudian1618 <1761844211@qq.com>
2019-07-22 16:17:22 +0800

    新建

--- src/com/github/gudian1618/Java_3/Dog.java
+++ src/com/github/gudian1618/Java_3/Dog.java
@@ -9,7 +9,10 @@ import java.io.Serializable;
  * @date 2019-07-22 15:45
  * @description
  * 如果一个类需要被序列化，该类必须实现Serializable接口
- * 就是一个标志接口，没有任何定义，为了告诉JVM该类对象可以被序列化
+ * 就是一个标志接口，没有任何定义，为了告诉JVM该类对象可以被序列化，交给JVM处理
+ * 神魔时候对象需要被序列化？
+ * 1、把对象保存到文件中（存储到物理介质中）
+ * 2、对象需要在网络上传输时
  */
 
 public class Dog implements Serializable {
--- src/com/github/gudian1618/Java_3/ObjectStreamDemo.java
+++ src/com/github/gudian1618/Java_3/ObjectStreamDemo.java
@@ -8,10 +8,32 @@ import java.io.*;
  * @version 1.0
  * @date 2019-07-22 15:38
  * @description
+ * 对象序列化
+ * 把对象写入文件：实际写入的时类名、属性名、属性类型、属性的值等。
+ * 反序列化
+ * 从文件中，把文件的内容读取出来还原成对象
  *
  */
 
 public class ObjectStreamDemo {
+
+    private static void readObject() {
+        File file = new File("/Users/zyd/IdeaProjects/Test/src/com/github" +
+            "/gudian1618/Java_3/dog.obj");
+        try {
+            InputStream in = new FileInputStream(file);
+            ObjectInputStream ois = new ObjectInputStream(in);
+            Dog dog = (Dog)ois.readObject();
+            ois.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
 
 //    java.io.NotSerializableException 没有序列化异常
     private static void writerObject() {
@@ -23,7 +45,7 @@ public class ObjectStreamDemo {
             ObjectOutputStream oos = new ObjectOutputStream(out);
             oos.writeObject(dog);
             oos.close();
-
+            System.out.println(dog);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
@@ -33,5 +55,6 @@ public class ObjectStreamDemo {
 
     public static void main(String[] args) {
         writerObject();
+        readObject();
     }
 }
