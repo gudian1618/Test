3a74cdd7d0d6e11dd635bc5dcaec6503111389c1
gudian1618 <1761844211@qq.com>
2019-08-13 11:19:24 +0800

    新建

--- src/com/github/gudian1618/Java_3/FileDivisionMergeDemo.java
+++ src/com/github/gudian1618/Java_3/FileDivisionMergeDemo.java
@@ -1,6 +1,6 @@
 package com.github.gudian1618.Java_3;
 
-import java.io.File;
+import java.io.*;
 
 /**
  * @author gudian1618
@@ -10,7 +10,20 @@ import java.io.File;
 public class FileDivisionMergeDemo {
     
     private static void division(File targetFile, long cutSize) {
-    
+        if(targetFile==null) return;
+        // 计算总分割的文件
+        int num = (targetFile.length()%cutSize==0)?(int)(targetFile.length()/cutSize):(int)(targetFile.length()/cutSize+1);
+        // 构造一个文件输入流
+        try {
+            BufferedInputStream in = new BufferedInputStream(new FileInputStream(targetFile));
+            BufferedOutputStream out = null;
+            
+            byte[] bytes = null;
+            
+            
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
     
     private static void merge() {
