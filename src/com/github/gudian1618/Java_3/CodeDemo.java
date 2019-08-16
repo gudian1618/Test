2f750622a0b98ea1bf9d2d747e3db38bb776e045
gudian1618 <1761844211@qq.com>
2019-08-16 16:51:15 +0800

    新建

---
+++ src/com/github/gudian1618/Java_3/CodeDemo.java
@@ -0,0 +1,25 @@
+package com.github.gudian1618.Java_3;
+
+import java.io.UnsupportedEncodingException;
+
+/**
+ * @author gudian1618
+ * @version v1.0
+ * @date 2019/8/16 16:41
+ * 通常产生乱码的情况是，两个不兼容的编码相互转换
+ */
+public class CodeDemo {
+    
+    public static void main(String[] args) {
+        String info = "小河流水哗啦啦";
+        try {
+            String newInfo = new String(info.getBytes("gb2312"),"iso8859-1");
+            System.out.println(newInfo);
+    
+            String newInfo2 = new String(newInfo.getBytes("iso8859-1"),"utf-8");
+            System.out.println(newInfo2);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+    }
+}
