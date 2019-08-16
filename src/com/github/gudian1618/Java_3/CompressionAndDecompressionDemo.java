e6f52f770a6bce3e77e0b3debddf304592bcfe50
gudian1618 <1761844211@qq.com>
2019-08-16 15:02:41 +0800

    新建

--- src/com/github/gudian1618/Java_3/CompressionAndDecompressionDemo.java
+++ src/com/github/gudian1618/Java_3/CompressionAndDecompressionDemo.java
@@ -8,12 +8,12 @@ import java.util.zip.ZipOutputStream;
  * @author gudian1618
  * @version v1.0
  * @date 2019/8/14 14:41
- *压缩与解压缩
-*/
+ * 压缩与解压缩
+ */
 public class CompressionAndDecompressionDemo {
     /*
-    * 压缩
-    * */
+     * 压缩
+     * */
     private static void compression(String zipFileName, File targetFile) {
         System.out.println("正在压缩。。。");
         try {
@@ -31,31 +31,29 @@ public class CompressionAndDecompressionDemo {
         }
     }
     
-    private static void zip(ZipOutputStream zOut, File targetFile, BufferedOutputStream bos, String name) {
+    private static void zip(ZipOutputStream zOut, File targetFile, BufferedOutputStream bos, String name) throws IOException {
         if (targetFile.isDirectory()) {
             File[] files = targetFile.listFiles();
-            if (files.length==0) {
-                try {
-                    // 处理空目录
-                    zOut.putNextEntry(new ZipEntry(name+"/"));
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                for (File f : files) {
-                    zip(zOut, f, name+"/"+f.getName(),bos);
-                }
+            if (files.length == 0) {
+                // 处理空目录
+                zOut.putNextEntry(new ZipEntry(name + "/"));
             }
+            for (File f : files) {
+                zip(zOut, f, bos, name + "/" + f.getName());
+            }
+        } else {
+            zOut.putNextEntry(new ZipEntry(name));
         }
     }
     
     /*
-    * 解压
-    * */
+     * 解压
+     * */
     private static void decompression() {
     
     }
     
     public static void main(String[] args) {
-        compression("F:\\ProgramData\\Test\\src\\com\\github\\gudian1618\\Java_3\\FileDivisionMergeDemo.java",new File("F:\\ProgramData\\Test\\src\\com\\github\\gudian1618\\Java_3\\"));
+        compression("F:\\ProgramData\\Test\\src\\com\\github\\gudian1618\\Java_3\\FileDivisionMergeDemo.java", new File("F:\\ProgramData\\Test\\src\\com\\github\\gudian1618\\Java_3\\"));
     }
 }
