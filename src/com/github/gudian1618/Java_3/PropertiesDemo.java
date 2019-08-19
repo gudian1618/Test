e61d364a2dbe52438766bec3cc497d667ad5967d
gudian1618 <1761844211@qq.com>
2019-08-19 15:17:05 +0800

    新建

--- src/com/github/gudian1618/Java_3/PropertiesDemo.java
+++ src/com/github/gudian1618/Java_3/PropertiesDemo.java
@@ -9,7 +9,7 @@ import java.util.Properties;
  * @date 2019/8/19 11:25
  * properties类可以用来做配置文件
  * javaweb javase 开发中通常使用
- *
+ * @description
  */
 public class PropertiesDemo {
     
@@ -22,8 +22,11 @@ public class PropertiesDemo {
 //        readConfig();
     }
     
-    /*
+    /**
     * 对属性文件的写入操作
+     * @param version
+     * @param username
+     * @param password
     * */
     
     private static void writeConfig(String version, String username, String password) {
@@ -31,9 +34,8 @@ public class PropertiesDemo {
         p.put("app.version", version);
         p.put("db.username", username);
         p.put("db.password", password);
-        FileOutputStream out = null;
         try {
-            out = new FileOutputStream("config.properties");
+            FileOutputStream out = new FileOutputStream("config.properties");
 //            写文件
             p.store(out, "update condig");
             out.close();
@@ -46,9 +48,10 @@ public class PropertiesDemo {
     
     private static void readConfig() {
         Properties p = new Properties();
-        
         try {
-           InputStream inStream = new FileInputStream("config.properties");
+           InputStream inStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("com/github/gudian1618/java_3/config.properties");
+//           InputStream inStream = new FileInputStream("config.properties");
+//            加载文件
             p.load(inStream);
             
 //            从properties中获取数据
@@ -66,8 +69,8 @@ public class PropertiesDemo {
     
     public static void main(String[] args) {
         
-        writeConfig("2.0", "mendeersong", "12345678");
-//        readConfig();
+//        writeConfig("2.0", "mendeersong", "12345678");
+        readConfig();
         System.out.println(PropertiesDemo.version);
         System.out.println(PropertiesDemo.username);
         System.out.println(PropertiesDemo.password);
