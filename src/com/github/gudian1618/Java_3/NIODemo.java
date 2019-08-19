fc397a31083220f6e47a96e18e43c8f03c6ad933
gudian1618 <1761844211@qq.com>
2019-08-19 10:17:43 +0800

    新建

---
+++ src/com/github/gudian1618/Java_3/NIODemo.java
@@ -0,0 +1,31 @@
+package com.github.gudian1618.Java_3;
+
+import java.nio.ByteBuffer;
+
+/**
+ * @author gudian1618
+ * @version v1.0
+ * @date 2019/8/19 10:12
+ */
+public class NIODemo {
+    
+    public static void main(String[] args) {
+//        创建一个字节缓冲区，申请内存空间为8字节
+        ByteBuffer buf = ByteBuffer.allocate(8);
+    
+        System.out.println("position="+buf.position());
+        System.out.println("limit="+buf.limit());
+        System.out.println("capacity="+buf.capacity());
+        System.out.println("========================");
+        
+//        向缓冲区中写入数据
+        buf.put((byte)10);
+        buf.put((byte) 20);
+        buf.put((byte) 30);
+        buf.put((byte) 40);
+        System.out.println("position="+buf.position());
+        System.out.println("limit="+buf.limit());
+        System.out.println("capacity="+buf.capacity());
+        System.out.println("========================");
+    }
+}
