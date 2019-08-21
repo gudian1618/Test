64dd0f58034ec4091ac8a9d931c431351627f35e
gudian1618 <1761844211@qq.com>
2019-08-21 16:44:54 +0800

    新建

--- src/com/github/gudian1618/Java_4/ThreadDemo4.java
+++ src/com/github/gudian1618/Java_4/ThreadDemo4.java
@@ -4,6 +4,8 @@ package com.github.gudian1618.Java_4;
  * @author gudian1618
  * @version v1.0
  * @date 2019/8/21 16:40
+ * 线程同步
+ *
  */
 public class ThreadDemo4 {
     
@@ -13,8 +15,21 @@ public class ThreadDemo4 {
 }
 
 class MyRunnable5 implements Runnable {
+    
+    private int ticket = 10; // 售票
+    
     @Override
     public void run() {
-    
+        for (int i = 0; i < 300; i++) {
+            if (ticket>0) {
+                System.out.println("您能够买的票剩余");
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
+
