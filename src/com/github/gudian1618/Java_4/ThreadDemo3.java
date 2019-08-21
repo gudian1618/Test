05942cd180da772a73ed396471b283d96c406dbe
gudian1618 <1761844211@qq.com>
2019-08-21 15:34:16 +0800

    新建

--- src/com/github/gudian1618/Java_4/ThreadDemo3.java
+++ src/com/github/gudian1618/Java_4/ThreadDemo3.java
@@ -5,6 +5,7 @@ package com.github.gudian1618.Java_4;
  * @version v1.0
  * @date 2019/8/21 15:15
  * 守护线程
+ * 线程可以分为守护线程和用户线程，当进程中没有用户线程时，JVM会退出
  */
 public class ThreadDemo3 {
     
@@ -22,6 +23,9 @@ public class ThreadDemo3 {
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
+            if (i==5) {
+                Thread.yield(); // 让出本次CPU执行时间片，下次继续
+            }
         }
     }
 }
@@ -37,6 +41,5 @@ class MyRunnable4 implements Runnable {
                 e.printStackTrace();
             }
         }
-        
     }
 }
