02429c7d2409bb7a3a9c13c0b244ab0edecf4644
gudian1618 <1761844211@qq.com>
2019-08-21 15:25:10 +0800

    新建

--- src/com/github/gudian1618/Java_4/ThreadDemo3.java
+++ src/com/github/gudian1618/Java_4/ThreadDemo3.java
@@ -4,13 +4,23 @@ package com.github.gudian1618.Java_4;
  * @author gudian1618
  * @version v1.0
  * @date 2019/8/21 15:15
+ * 守护线程
  */
 public class ThreadDemo3 {
     
     public static void main(String[] args) {
         MyRunnable4 mr4 = new MyRunnable4();
-        Thread t4 = new Thread();
+        Thread t4 = new Thread(mr4);
         t4.start();
+        
+        for (int i = 0; i < 50; i++) {
+            System.out.println("main-" + i);
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
 
@@ -18,7 +28,12 @@ class MyRunnable4 implements Runnable {
     @Override
     public void run() {
         for (int i = 0; i < 50; i++) {
-            System.out.println("---"+i);
+            System.out.println("---" + i);
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
         
     }
