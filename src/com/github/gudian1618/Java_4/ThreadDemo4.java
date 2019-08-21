cb399c860fd0a7313d15999060f7b535ee1ed096
gudian1618 <1761844211@qq.com>
2019-08-21 16:41:57 +0800

    新建

---
+++ src/com/github/gudian1618/Java_4/ThreadDemo4.java
@@ -0,0 +1,20 @@
+package com.github.gudian1618.Java_4;
+
+/**
+ * @author gudian1618
+ * @version v1.0
+ * @date 2019/8/21 16:40
+ */
+public class ThreadDemo4 {
+    
+    public static void main(String[] args) {
+    
+    }
+}
+
+class MyRunnable5 implements Runnable {
+    @Override
+    public void run() {
+    
+    }
+}
