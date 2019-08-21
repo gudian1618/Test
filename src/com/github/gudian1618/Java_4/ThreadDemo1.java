ab8b1382c227e66839fa829ab0ef23d2907ee998
gudian1618 <1761844211@qq.com>
2019-08-21 10:21:09 +0800

    新建

--- src/com/github/gudian1618/Java_4/ThreadDemo1.java
+++ src/com/github/gudian1618/Java_4/ThreadDemo1.java
@@ -8,11 +8,15 @@ package com.github.gudian1618.Java_4;
 public class ThreadDemo1 {
     
     public static void main(String[] args) {
+        
         MyThread mt = new MyThread();
         mt.start(); // 启动线程
+        
+        MyRunnable mr = new MyRunnable();
+        Thread t2 = new Thread(mr);
+        t2.start();
     }
 }
-
 
 /**
  * 实现线程的第一种方式：继承Thread类
@@ -21,7 +25,18 @@ class MyThread extends Thread {
     @Override
     public void run() {
         for (int i = 0; i < 50; i++) {
-            System.out.println(i);
+            System.out.println(Thread.currentThread().getName() + "-" + i);
+        }
+    }
+}
+
+/**
+ * 实现接口的第二种方式，实现Runnable接口
+ */
+class MyRunnable implements Runnable {
+    public void run() {
+        for (int i = 0; i < 50; i++) {
+            System.out.println(Thread.currentThread().getName() + "-" + i);
         }
     }
 }
