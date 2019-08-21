f2ea75e018a3dc5eb9affff986aabd7a59209cb9
gudian1618 <1761844211@qq.com>
2019-08-21 15:27:04 +0800

    新建

--- src/com/github/gudian1618/Java_4/ThreadDemo3.java
+++ src/com/github/gudian1618/Java_4/ThreadDemo3.java
@@ -11,6 +11,8 @@ public class ThreadDemo3 {
     public static void main(String[] args) {
         MyRunnable4 mr4 = new MyRunnable4();
         Thread t4 = new Thread(mr4);
+//        线程可以分为守护线程和用户线程，当进程中没有用户线程时，JVM会退出
+        t4.setDaemon(true); // 把线程设置为守护线程
         t4.start();
         
         for (int i = 0; i < 50; i++) {
