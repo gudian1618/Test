1f72971d92ac4721afe743132736af2f4ce0c9c6
gudian1618 <1761844211@qq.com>
2019-08-21 16:37:35 +0800

    新建

--- src/com/github/gudian1618/Java_4/ThreadDemo3.java
+++ src/com/github/gudian1618/Java_4/ThreadDemo3.java
@@ -12,9 +12,14 @@ public class ThreadDemo3 {
     public static void main(String[] args) {
         MyRunnable4 mr4 = new MyRunnable4();
         Thread t4 = new Thread(mr4);
+        t4.setName("Thread-t");
 //        线程可以分为守护线程和用户线程，当进程中没有用户线程时，JVM会退出
+//      优先级可以提高该线程抢占CPU时间片的概率
+        t4.setPriority(Thread.MAX_PRIORITY);
         t4.setDaemon(true); // 把线程设置为守护线程
+        System.out.println(t4.isAlive()); // 是否处于激活状态
         t4.start();
+        System.out.println(t4.isAlive());
         
         for (int i = 0; i < 50; i++) {
             System.out.println("main-" + i);
