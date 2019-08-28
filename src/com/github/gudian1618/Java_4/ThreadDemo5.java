e206ef34ee72d578f2419e3ebccc6abf94352e0c
gudian1618 <1761844211@qq.com>
2019-08-28 16:46:22 +0800

    新建

--- src/com/github/gudian1618/Java_4/ThreadDemo5.java
+++ src/com/github/gudian1618/Java_4/ThreadDemo5.java
@@ -2,6 +2,8 @@ package com.github.gudian1618.Java_4;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author gudian1618
@@ -11,10 +13,37 @@ import java.util.concurrent.Executors;
  */
 public class ThreadDemo5 {
 
+    private static ExecutorService executorService;
+
     public static void main(String[] args) {
+        // 创建线程池（4种）
+        // 1、创建一个单线程的线程池
         ExecutorService es = Executors.newSingleThreadExecutor();
+
+        // 2.创建固定大小的线程池
+        ExecutorService es1 = Executors.newFixedThreadPool(1);
+
+        // 3、创建一个可缓存的线程池
+        ExecutorService es2 = Executors.newCachedThreadPool();
+
+        // 4、创建一个
+        ScheduledExecutorService es3 = Executors.newScheduledThreadPool(3);
+
         es.execute(new MyRunable6());
         es.execute(new MyRunable6());
+
+        es1.execute(new MyRunable6());
+        es1.execute(new MyRunable6());
+
+        es2.execute(new MyRunable6());
+        es2.execute(new MyRunable6());
+
+        es3.schedule(new MyRunable6(), 3000, TimeUnit.MINUTES);
+
+        es.shutdown();
+        es1.shutdown();
+        es2.shutdown();
+        es3.shutdown();
     }
 }
 
