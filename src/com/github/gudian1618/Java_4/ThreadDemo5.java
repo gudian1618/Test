3bfaff221f42e5b6c49275c48db63abcefa4cbea
gudian1618 <1761844211@qq.com>
2019-09-06 16:51:02 +0800

    新建

--- src/com/github/gudian1618/Java_4/ThreadDemo5.java
+++ src/com/github/gudian1618/Java_4/ThreadDemo5.java
@@ -60,5 +60,4 @@ class MyRunable6 implements Runnable {
             }
         }
     }
-
 }
--- src/com/github/gudian1618/Java_5/ListDemo.java
+++ src/com/github/gudian1618/Java_5/ListDemo.java
@@ -17,15 +17,32 @@ import java.util.List;
  */
 public class ListDemo {
 
-    private void arrayList() {
-        List list = new ArrayList();
+    private static void arrayList() {
+        // 使用集合来存储多个不同类型的元素对象，那么在处理时比较麻烦，在实际开发中不建议这样使用。
+        // 在一个集合中存储相同类型的对象
+        List<String> list = new ArrayList<>();
         list.add("李老师");
         list.add("张老师");
         list.add("赵老师");
         list.add("苍老师");
+        // 遍历集合
+        int size = list.size();
+        // 这样效率高，计算性能高，只计算一次，新建对象,还有更高效的方法。
+        for (int i = 0; i < size; i++) {
+            System.out.println(list.get(i));
+        }
+
+        System.out.println(list.contains("苍老师"));
+        list.remove("赵老师");
+        System.out.println(list.size());
+
+//        String[] array = list.toArray(new String[]{});
+//        for (String s: array) {
+//            System.out.println(s);
+//        }
     }
 
     public static void main(String[] args) {
-
+        arrayList();
     }
 }
